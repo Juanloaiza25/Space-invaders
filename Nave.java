0dff575cedc7c6ea62272717ad162a9049ed950b
Juan Diego <juan.nino1@utp.edu.co>
2024-09-18 17:39:54 -0500

    Actualizado SpaceShooter

--- Nave.java
+++ Nave.java
@@ -18,6 +18,43 @@ public class Nave extends KeyAdapter {
 
     // Métodos
 
+    public void dibujar(Graphics g) {
+        g.setColor(Color.WHITE);
+        int[] xPoints = { this.x, this.x + this.ancho / 2, this.x + this.ancho }; //
+        int[] yPoints = { this.y + this.alto, this.y, this.y + this.alto };
+        g.fillPolygon(xPoints, yPoints, 3);
+    }
+
+    // Metodo para mover a la izquierda, este comprueba las colisiones de la
+    // ventana, si x es menor a 0, se establece en 0
+
+    public void moverIzquierda() {
+        this.x -= this.velocidad;
+        if (this.x < 0) {
+            this.x = 0;
+        }
+    }
+
+    // Metodo para mover a la derecha, este comprueba las colisiones de la ventana,
+    // si x es mayor a 750, se establece en 750
+
+    public void moverDerecha() {
+        this.x += this.velocidad;
+        if (this.x > 750) {
+            this.x = 750;
+        }
+    }
+
+    @Override // Método de la clase KeyAdapter
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_LEFT || e.getKeyCode() == KeyEvent.VK_A) {
+            moverIzquierda(); // Mover la nave a la izquierda usando la flecha izquierda o la tecla 'A'
+        } else if (e.getKeyCode() == KeyEvent.VK_RIGHT || e.getKeyCode() == KeyEvent.VK_D) {
+            moverDerecha(); // Mover la nave a la derecha usando la flecha derecha o la tecla 'D'
+        } else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+            disparando = true;
+        }
+    }
 
     public int getX() {
         return x;
@@ -38,50 +75,4 @@ public class Nave extends KeyAdapter {
     public boolean isDisparando() {
         return disparando;
     }
-
-    
-    public void dibujar(Graphics g) {
-        g.setColor(Color.WHITE);
-        int[] xPoints = {this.x, this.x + this.ancho / 2 , this.x + this.ancho}; // 
-        int[] yPoints = {this.y + this.alto , this.y, this.y + this.alto};
-        g.fillPolygon(xPoints, yPoints, 3);
-    }
-    
-    //Metodo para mover a la izquierda, este comprueba las colisiones de la ventana, si x es menor a 0, se establece en 0
-    
-    public void moverIzquierda() {
-        this.x -= this.velocidad;
-        if (this.x < 0) {
-            this.x = 0;
-        }
-    }
-    
-    //Metodo para mover a la derecha, este comprueba las colisiones de la ventana, si x es mayor a 750, se establece en 750
-    
-    public void moverDerecha() {
-        this.x += this.velocidad;
-        if (this.x > 750) {
-            this.x = 750;
-        }
-    }
-    
-    @Override // Método de la clase KeyAdapter
-    public void keyPressed(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_LEFT || e.getKeyCode() == KeyEvent.VK_A) {
-            moverIzquierda(); // Mover la nave a la izquierda usando la flecha izquierda o la tecla 'A'
-        } else if (e.getKeyCode() == KeyEvent.VK_RIGHT || e.getKeyCode() == KeyEvent.VK_D) {
-            moverDerecha(); // Mover la nave a la derecha usando la flecha derecha o la tecla 'D'
-        } else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-            disparando = true;
-        }
-    }
 }
-
-
-
-
-
- 
-
-
-    
